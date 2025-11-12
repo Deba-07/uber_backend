e219faa1d4b2ea255387de816f50e034fc1a4dc5
Deba-07 <dynamicdeba07@gmail.com>
2025-11-12 15:43:19 +0530

    Refactor app.js and server.js for improved structure and readability

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const dotenv = require('dotenv')
-dotenv.config()
-const express = require('express')
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
 const app = express();
-const cors = require('cors')
-app.use(cors())
+const cors = require("cors");
+const connectToDb = require("./db/db");
 
-app.get('/', (req, res) => {
-    res.send("Welcome to Uber to backend API.")
-})
+const userRoutes = require("./routes/user.routes");
+
+connectToDb();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Uber to backend API.");
+});
+
+app.use("/users", userRoutes);
 
 module.exports = app;
